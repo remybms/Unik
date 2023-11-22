9c64b7dfa35b6e48bc1f2c3ca7c8bca5c9628341
remybms <remy.bamas@ynov.com>
2023-11-22 15:03:57 +0100

    create header

---
+++ components/header.js
@@ -0,0 +1,10 @@
+import styles from '../styles/header.module.css'
+
+export default function Header(){
+    return(
+        <><div className={styles.all}>
+            <img src='/img/UNIK.jpg' width={100} height={100}/>
+            <div>HEAAAAAAADER</div>
+        </div></>
+    )
+}
