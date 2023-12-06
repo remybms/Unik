aa36a4bd560d2ffac8df1cb781d11d2870fd159e
remybms <remy.bamas@ynov.com>
2023-12-06 16:09:04 +0100

    add links on the footer, wrong colors

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import styles from "../styles/footer.module.css"
-import { BiLogoInstagram} from "react-icons/bi"
+import { BiLogoInstagram } from "react-icons/bi"
 
 export default function Footer() {
     return (
@@ -15,9 +15,16 @@ export default function Footer() {
                             <div>L'atelier personnalisation</div>
                         </div>
                         <div>
-                            <div>Mentions légales</div>
-                            <div>Politique de confidentialité</div>
-                            <div>CGV</div>
+                            <div>
+                                <Link href="https://unik-sport.com/?page_id=422">Mentions légales</Link>
+                            </div>
+                            <div>
+                                <Link href="https://unik-sport.com/?page_id=422">Politique de confidentialité</Link>
+                            </div>
+                            <div>
+                                <Link href="https://unik-sport.com/?page_id=422">CGV</Link>
+                            </div>
+
                         </div>
                     </div>
 
@@ -27,8 +34,8 @@ export default function Footer() {
                     <div>Ne manquez pas nos promotions ! Abonnez-vous dès aujourd'hui !</div>
                 </div>
                 <div>Spécialiste dans la customisation de vêtements pour sportifs particuliers et professionnels.</div>
-                <Link href="https://www.instagram.com/_unik.sport/"><BiLogoInstagram size={42}/></Link>
-                <div>©2023. UNIK. Par LGDESIGN</div>
+                <Link href="https://www.instagram.com/_unik.sport/"><BiLogoInstagram size={42} /></Link>
+                <Link href="https://lg-design.fr/">©2023. UNIK. Par LGDESIGN</Link>
             </div>
         </>
     )
