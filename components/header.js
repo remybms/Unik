6da4e1b87dbc818138ad854dbbd2640610b2c664
remybms <remy.bamas@ynov.com>
2023-12-06 11:44:54 +0100

    making full header

--- components/header.js
+++ components/header.js
@@ -1,10 +1,29 @@
 import styles from '../styles/header.module.css'
+import Hamburger from './hamburger'
+import Link from 'next/link'
+import { auth } from "../config/firebase"
+import { CgProfile } from 'react-icons/cg'
+
+
 
 export default function Header(){
+    const user = auth.currentUser;
     return(
         <><div className={styles.all}>
-            <img src='/img/UNIK.jpg' width={100} height={100}/>
-            <div>HEAAAAAAADER</div>
+            <img src='/img/UNIK.webp' width={198} height={37}/>
+            <div className={styles.links}>
+                <Link href="/" className={styles.link}>Accueil</Link>
+                <div className={styles.link}>Boutique</div>
+                <div className={styles.link}>Atelier</div>
+            </div>
+            <div className={styles.cartButton}>
+                <Hamburger />
+            </div>
+            {user ?
+                    <Link href="/profile"><CgProfile size={42} /></Link>
+                    :
+                    <Link as="/connexion" href="/Auth/Login"><CgProfile size={42} /></Link>
+            }
         </div></>
     )
 }
