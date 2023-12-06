1da9daf9dea8480722d2c0e265fa80c91fb5fc1f
remybms <remy.bamas@ynov.com>
2023-12-06 10:38:01 +0100

    hamburger menu for cart

---
+++ components/hamburger.js
@@ -0,0 +1,37 @@
+import { slide as Menu} from'react-burger-menu'
+import { FaShoppingCart } from 'react-icons/fa'
+import { Cart } from './cart'
+
+function Hamburger(){
+    var styles ={
+        bmBurgerButton: {
+          position: 'absolute',
+          width: '42px',
+          height: '42px',
+        },
+        bmMenuWrap: {
+            width: '40%',
+            height: '90%',
+            marginTop: '10%',
+            padding: '5%',
+            backgroundColor: '#FFCC85',
+            borderRadius: '10px',
+            color: '#262626'
+        },
+        bmCrossButton: {
+            height: '24px',
+            width: '24px',
+        },
+        bmCross: {
+            background: '#262626',
+        },
+    }
+    return(
+        <div>
+            <Menu customBurgerIcon={<FaShoppingCart size={42} />} styles={styles} menuClassName='w-28'>
+                <Cart/>
+            </Menu>
+        </div>
+    )
+}
+export default Hamburger
