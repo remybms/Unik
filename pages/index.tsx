11d5ed894a4d7df070e4b6ad4af2f60327a0089b
remybms <remy.bamas@ynov.com>
2023-12-06 16:00:48 +0100

    add footer on main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
 import Header from '../components/header'
+import Footer from '../components/footer'
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 
 type ConnectionStatus = {
@@ -95,17 +96,6 @@ export default function Home({
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
         .container {
           min-height: 100vh;
@@ -121,25 +111,6 @@ export default function Home({
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
           justify-content: center;
           align-items: center;
         }
@@ -255,6 +226,7 @@ export default function Home({
           box-sizing: border-box;
         }
       `}</style>
-    </div></>
+    </div>
+    <Footer/></>
   )
 }
